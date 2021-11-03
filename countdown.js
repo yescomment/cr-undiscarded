af0663ba4d7156d3c6188334a2c0f69f734ea16e
Jacob Ford <j@cobford.com>
2021-11-02 23:48:25 -0400

    Update top content

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-const eventDate = new Date('Mar 21 2020 21:00:00 EDT')
+const eventDate = new Date('Jan 14 2022 12:00:00 EDT')
 const element = document.getElementById('door')
 
 function updateCountdown() {
